d5888cb29a0631d7ae8e2e42e52678f985e63541
stefaniaMartinez17 <sharidmber@gmail.com>
2023-06-05 17:04:01 -0500

    v13

--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -7,9 +7,16 @@ export const Navbar = () => {
   const { logout } = useAuth0();
   return (
     <div className="navbar">
+      <div className="logo">
+        <img
+          src="./../../public/img/LOGO_ECCOMERCE2.png"
+          alt="Logo de la empresa"
+        ></img>
+        <p>DecoMuebles</p>
+      </div>
       <div className="links">
         {/*links para navegar en la pagina*/}
-        <Link to="/shop"> Tienda </Link>
+        <Link to="/shop">Tienda</Link>
         {/*to="" donde vas a ir cuando das click en el link*/}
         <Link to="/cart"> Carrito </Link>
       </div>
@@ -19,7 +26,7 @@ export const Navbar = () => {
             logout({ logoutParams: { returnTo: window.location.origin } })
           }
         >
-          Log Out
+          LogOut
         </button>
       </div>
     </div>
